6e608080b90ef2b5fb336f4ce0bd4db44bd65000
Denny Zhang <dennyz@umich.edu>
2017-04-05 18:15:17 -0400

    initial commit, hopefully that's enough files to work

---
+++ Dualshock/SoftConsole/Dualshock_MSS_MSS_CM3_0/Dualshock_MSS_MSS_CM3_0_hw_platform/Dualshock_hw_platform.h
@@ -0,0 +1,17 @@
+#ifndef Dualshock_HW_PLATFORM_H_
+#define Dualshock_HW_PLATFORM_H_
+/*****************************************************************************
+*
+*Created by Microsemi SmartDesign  Wed Apr 05 17:35:20 2017
+*
+*Memory map specification for peripherals in Dualshock
+*/
+
+/*-----------------------------------------------------------------------------
+* MSS_CM3_0 subsystem memory map
+* Master(s) for this subsystem: MSS_CM3_0 
+*---------------------------------------------------------------------------*/
+#define CORESPI_0                       0x40000000U
+
+
+#endif /* Dualshock_HW_PLATFORM_H_*/
